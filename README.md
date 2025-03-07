d8e3e1a2abcded661cecbbd32363f8d2f7a86ea9
Sean Corley <200403627+scorley627@users.noreply.github.com>
2025-03-07 11:22:26 -0700

    Add style with css

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Rock Paper Scissors
 
-A simple rock paper scissors game that runs in the console to show off some
-basic javascript knowledge.
+A simple rock paper scissors game with a basic UI to show off some recently
+learned javascript skills. Includes the use of a click event listener to get
+the user's move and dynamically updates the page to display the outcome.
 
 Based on the assignment from The Odin Project foundations course.
--- javascript.js
+++ javascript.js
@@ -1,39 +1,37 @@
 let humanScore = 0;
 let computerScore = 0;
+let gameOver = false;
 
 document.addEventListener("click", onClick);
 
 function onClick(event) {
-  if (event.target.tagName == "BUTTON") {
-    playRound(event.target.className, getComputerChoice());
+  const buttonClicked = event.target.tagName == "BUTTON";
+  const playAgainClicked = event.target.textContent == "Play Again";
 
-    if (humanScore == 5) {
-      const gameOverDiv = document.createElement("div");
-      gameOverDiv.textContent = "Game Over! You win!";
-
-      const resultsDiv = document.querySelector(".results");
-      resultsDiv.appendChild(gameOverDiv);
-
-      document.removeEventListener("click", onClick);
-    } else if (computerScore == 5) {
-      const gameOverDiv = document.createElement("div");
-      gameOverDiv.textContent = "Game Over! You lose!";
-
-      const resultsDiv = document.querySelector(".results");
-      resultsDiv.appendChild(gameOverDiv);
-
-      document.removeEventListener("click", onClick);
-    }
+  if (playAgainClicked) {
+    location.reload();
+  } else if (buttonClicked && !gameOver) {
+    const humanChoice = event.target.className;
+    playRound(humanChoice);
+    checkGameOver();
   }
 }
 
-function playRound(humanChoice, computerChoice) {
-  let winner = getWinner(humanChoice, computerChoice);
+function playRound(humanChoice) {
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(humanChoice, computerChoice);
   updateScore(winner);
   
-  let resultsText = getResultsText(
+  const resultsText = getResultsText(
     winner, capitalize(humanChoice), capitalize(computerChoice));
   displayResults(resultsText);
+}
+
+function checkGameOver() {
+  gameOver = humanScore == 5 || computerScore == 5;
+  if (gameOver) {
+    displayGameOver();
+  }
 }
 
 function getWinner(humanChoice, computerChoice) {
@@ -56,13 +54,11 @@ function getWinner(humanChoice, computerChoice) {
 
 function updateScore(winner) {
   if (winner == "human") {
-    ++humanScore;
     const scoreDiv = document.querySelector(".human-score");
-    scoreDiv.textContent = `You - ${humanScore}`;
+    scoreDiv.textContent = `You - ${++humanScore}`;
   } else if (winner == "computer") {
-    ++computerScore;
     const scoreDiv = document.querySelector(".computer-score");
-    scoreDiv.textContent = `Computer - ${computerScore}`;
+    scoreDiv.textContent = `Computer - ${++computerScore}`;
   }
 }
 
@@ -78,14 +74,29 @@ function getResultsText(winner, humanChoice, computerChoice) {
 }
 
 function displayResults(resultsText) {
-  const resultsDiv = document.querySelector(".results");
   const textDiv = document.createElement("div");
   textDiv.textContent = resultsText;
+
+  const resultsDiv = document.querySelector(".results");
   resultsDiv.appendChild(textDiv);
 }
 
+function displayGameOver() {
+  const gameOverDiv = document.createElement("div");
+  gameOverDiv.className = "game-over";
+  gameOverDiv.textContent =
+    humanScore == 5 ? "Game Over! You win!" : "Game Over! You lose!"
+
+  const replayButton = document.createElement("button");
+  replayButton.textContent = "Play Again";
+
+  const resultsDiv = document.querySelector(".results");
+  resultsDiv.appendChild(gameOverDiv);
+  resultsDiv.appendChild(replayButton);
+}
+
 function getComputerChoice() {
-  let num = Math.random();
+  const num = Math.random();
   if (0 <= num && num < 1 / 3) {
     return "rock";
   } else if (1 / 3 <= num && num < 2 / 3) {
